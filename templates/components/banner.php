9448b3669c750b42392571117837f843b5c7e5af
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-03-16 15:50:55 -0600

    upd: Se cambio pickups por camionetas

--- templates/components/banner.php
+++ templates/components/banner.php
@@ -10,7 +10,7 @@
         <div class="banner__links bottom8">
             <a href="taxis.php" class="banner__text" data-aos="fade-up" data-aos-delay="200">TAXIS</a>
             <a href="gruas.php" class="banner__text" data-aos="fade-up" data-aos-delay="500">GRÚAS</a>
-            <a href="pickups.php" class="banner__text" data-aos="fade-up" data-aos-delay="1000">PICK UPS</a>
+            <a href="camionetas.php" class="banner__text" data-aos="fade-up" data-aos-delay="1000">CAMIONETAS</a>
         </div>
     <?php elseif($archivo == 'taxis.php'): ?>
         <h1 class="banner__title bottom20 " data-aos="fade-up">> SERVICIO LAS 24 HORAS DEL DIA LOS 365 DÍAS DEL AÑO.</h1>
@@ -26,11 +26,11 @@
         <div class="banner__links bottom8">
             <a href="#taxis" class="banner__text" data-aos="fade-up" data-aos-delay="200">GRÚAS</a>
         </div>
-    <?php elseif($archivo == 'pickups.php'): ?>
+    <?php elseif($archivo == 'camionetas.php'): ?>
         <h1 class="banner__title bottom20 " data-aos="fade-up">> LA FORMA MÁS PRÁCTICA Y ECONÓMICA PARA TRANSPORTAR CUALQUIER COSA QUE NECESITES.</h1>
         <h2 class="banner__title bottom10 " data-aos="fade-down">871-718-1111</h2>
         <div class="banner__links bottom8">
-            <a href="#taxis" class="banner__text" data-aos="fade-up" data-aos-delay="200">PICK UPS</a>
+            <a href="#taxis" class="banner__text" data-aos="fade-up" data-aos-delay="200">CAMIONETAS</a>
         </div>
     <?php endif; ?>
 
@@ -41,7 +41,7 @@
         <img src="resources/servicios/banner.jpg " alt="Taxi" class="banner__image" loading="lazy">
     <?php elseif($archivo == 'gruas.php'): ?>
         <img src="resources/servicios/gruabanner.jpg " alt="Taxi" class="banner__image" loading="lazy">
-    <?php elseif($archivo == 'pickups.php'): ?>
+    <?php elseif($archivo == 'camionetas.php'): ?>
         <img src="resources/servicios/pickupbanner.jpg " alt="Taxi" class="banner__image" loading="lazy">
     <?php endif; ?>
 
@@ -55,6 +55,6 @@
             <div class="words">
                 <span>Taxis</span>
                 <span>Grúas</span>
-                <span>Pick Ups</span>
+                <span>CAMIONETAS</span>
             </div>
         </div>-->
--- templates/components/navbar.php
+++ templates/components/navbar.php
@@ -34,10 +34,10 @@
             <a href="https://www.facebook.com/Mandarinas/" target="blank"><i class="fa-brands fa-facebook navbar__icon"></i></a>
             <a class="navbar__menu btnAbrir" ><img src="resources/menu.png" alt="menu"></a>
         </div>
-    <?php elseif($archivo == 'pickups.php'): ?>
+    <?php elseif($archivo == 'camionetas.php'): ?>
         <div class="navbar__links" data-aos="fade-left">
             <a href="#" class="navbar__link">INICIO</a>
-            <a href="#pickups" class="navbar__link">PICK UPS</a>
+            <a href="#camionetas" class="navbar__link">PICK UPS</a>
             <a href="#contacto" class="navbar__link">CONTACTO</a>
             <a href="https://www.facebook.com/Mandarinas/" target="blank"><i class="fa-brands fa-facebook navbar__icon"></i></a>
             <a class="navbar__menu btnAbrir" ><img src="resources/menu.png" alt="menu"></a>
@@ -62,8 +62,8 @@
             <a href="#mandarinasplus" class="navbar-mini__link">MANDARINAS PLUS</a>
         <?php elseif($archivo == 'gruas.php'): ?>
             <a href="#gruas" class="navbar-mini__link">GRÚAS</a>
-        <?php elseif($archivo == 'pickups.php'): ?>
-            <a href="#pickups" class="navbar-mini__link">PICK UPS</a>
+        <?php elseif($archivo == 'camionetas.php'): ?>
+            <a href="#camionetas" class="navbar-mini__link">PICK UPS</a>
         <?php endif; ?>
 
         <a href="#contacto" class="navbar-mini__link">CONTACTO</a>
