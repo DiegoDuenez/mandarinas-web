3fb972af8a66139b22fe89eacfbbafe436b1083b
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-03-19 13:30:45 -0600

    add: fontawesome

--- taxis.php
+++ taxis.php
@@ -21,6 +21,7 @@
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!--====== AOS CSS ======-->
     <link rel="stylesheet" href="libs/aos/dist/aos.css">
+    <!--<link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.1.0/css/all.css.css">-->
     <!--====== MAIN CSS ======-->
     <link rel="stylesheet" href="css/settings/normalize.css">
     <link rel="stylesheet" href="css/settings/root.css">
